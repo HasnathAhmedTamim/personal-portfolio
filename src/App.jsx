809955c2ede0638154ee761297c1eed0148efbf0
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-01-14 14:10:30 +0600

    tailwind and npm stp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      {/* <h1 className="font-roboto text-5xl font-bold text-start text-primary">
+        Hasnath Ahmed says hello
+      </h1> */}
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
